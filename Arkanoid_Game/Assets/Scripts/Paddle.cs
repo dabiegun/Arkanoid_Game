2ab645ce3f0da0d7f359965db5a2a796d794b5a7
Dawid Biegun <dabiegun@gmail.com>
2020-01-26 21:54:58 +0100

    Move Paddle

---
+++ Arkanoid_Game/Assets/Scripts/Paddle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float paddleinitialY;
+    private float deflautPaddleWidthPixels = 200;
+    private float deflautLeftClamp = 135;
+    private float deflautRightClamp = 410;
+    private SpriteRenderer sr;
+
+    private void Start()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+        paddleinitialY = this.transform.position.y;
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        PaddleMovement();
+    }
+
+    private void PaddleMovement()
+    {
+        float paddleShift = (deflautPaddleWidthPixels - ((deflautPaddleWidthPixels / 2) * this.sr.size.x)) / 2;
+        float leftClamp = deflautLeftClamp - paddleShift;
+        float rightClamp = deflautRightClamp + paddleShift;
+        float mousePositionPixels = Mathf.Clamp(Input.mousePosition.x, leftClamp, rightClamp);
+        float mousePositionWorldX = mainCamera.ScreenToWorldPoint(new Vector3(mousePositionPixels, 0, 0)).x; 
+        this.transform.position = new Vector3(mousePositionWorldX, paddleinitialY, 0);
+    }
+}
